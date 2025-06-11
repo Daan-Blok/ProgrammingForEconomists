5cba1d8ed45fe47d8b6407c0cd7d88db425fb135
DriesBerns <driesberns@gmail.com>
2025-06-11 10:06:47 +0200

    .

--- TestingProject.R
+++ TestingProject.R
@@ -342,7 +342,6 @@ print(p2)
   
   print(p10)
   
-
   #plot mensen met eigen huis inkomen vs mensen huurwoning zonder toeslag vs mensen huurwoning met toeslag
   #kan nog OLS gooien op de time series voor extrepolatie
   #zelfde voor inkomen OLS kan ook en dan zeggen gat gaat alleen maar groter worden tussen de twee
